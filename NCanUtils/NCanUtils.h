caacbacbe726918bd7943fae15713f228085f1ac
Nguyen Can <can.nguyen@sugarventures.co>
2020-04-26 18:00:43 +0700

    Initilize source

---
+++ NCanUtils/NCanUtils.h
@@ -0,0 +1,19 @@
+//
+//  NCanUtils.h
+//  NCanUtils
+//
+//  Created by Nguyen Can on 4/26/20.
+//  Copyright © 2020 Nguyen Can. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NCanUtils.
+FOUNDATION_EXPORT double NCanUtilsVersionNumber;
+
+//! Project version string for NCanUtils.
+FOUNDATION_EXPORT const unsigned char NCanUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NCanUtils/PublicHeader.h>
+
+
